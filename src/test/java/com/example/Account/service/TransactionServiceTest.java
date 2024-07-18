85cb122104136d9ba62de68ef2d4cf28eb7e615c
soohomode <soohomode@gmail.com>
2024-07-19 03:15:44 +0900

    240719 잔액 사용 4/4

---
+++ src/test/java/com/example/Account/service/TransactionServiceTest.java
@@ -0,0 +1,249 @@
+package com.example.Account.service;
+
+import com.example.Account.domain.Account;
+import com.example.Account.domain.AccountUser;
+import com.example.Account.domain.Transaction;
+import com.example.Account.dto.TransactionDto;
+import com.example.Account.exception.AccountException;
+import com.example.Account.repository.AccountRepository;
+import com.example.Account.repository.AccountUserRepository;
+import com.example.Account.repository.TransactionRepository;
+import com.example.Account.type.AccountStatus;
+import com.example.Account.type.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static com.example.Account.type.AccountStatus.IN_USE;
+import static com.example.Account.type.TransactionResultType.F;
+import static com.example.Account.type.TransactionResultType.S;
+import static com.example.Account.type.TransactionType.USE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionServiceTest {
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Mock
+    private AccountUserRepository accountUserRepository;
+
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @Test
+    void successUseBalance() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+
+        // 따로 뽑은 계좌 정보
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(IN_USE)
+                .balance(10000L)
+                .accountNumber("1000000012").build();
+
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(USE)
+                        .transactionResultType(S)
+                        .transactionId("transactionId")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapshot(9000L)
+                        .build());
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        //when
+        TransactionDto transactionDto = transactionService.useBalance(
+                1L, "1000000000", 200L);
+
+        //then 10000원에서 1000원 썼으니 9000원 일것이다
+        verify(transactionRepository, times(1)).save(captor.capture());
+        assertEquals(200L, captor.getValue().getAmount());
+        assertEquals(9800L, captor.getValue().getBalanceSnapshot());
+
+        assertEquals(9000L, transactionDto.getBalanceSnapshot()); // 잔액
+        assertEquals(1000L, transactionDto.getAmount()); // 거래 금액
+        assertEquals(S, transactionDto.getTransactionResultType()); // 성공여부
+        assertEquals(USE, transactionDto.getTransactionType()); // 사용 or 취소
+    }
+
+    @Test
+    @DisplayName("해당 유저 없음 - 잔액 사용 실패💦")
+    void useBalance_UserNotFound() {
+        //given
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        //when
+        AccountException accountException = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1000000000", 1000L));
+
+        //then
+        assertEquals(ErrorCode.USER_NOT_FOUND, accountException.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("해당 계좌 없음 - 잔액 사용 실패")
+    void deleteAccount_AccountNotFound() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.empty());
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1000000000", 1000L));
+
+        //then
+        assertEquals(ErrorCode.ACCOUNT_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("계좌 소유주 다름 - 잔액 사용 실패")
+    void deleteAccountFailed_userUnMatch() {
+        //given
+        AccountUser Pobi = AccountUser.builder() // 유저 1
+                .id(12L)
+                .name("Pobi").build();
+        AccountUser Yuri = AccountUser.builder() // 유저 2
+                .id(13L)
+                .name("Yuri").build();
+        given(accountUserRepository.findById(anyLong())) // id
+                .willReturn(Optional.of(Pobi)); // 포비였는데
+        given(accountRepository.findByAccountNumber(anyString())) // 계좌번호
+                .willReturn(Optional.of(Account.builder()
+                        .accountUser(Yuri) // 계좌의 소유주는 유리이다
+                        .balance(0L)
+                        .accountNumber("1000000012").build()));
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        //then
+        assertEquals(ErrorCode.USER_ACCOUNT_UN_MATCH, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("해지된 계좌는 사용할 수 없습니다.")
+    void deleteAccountFailed_alreadyUnRegistered() {
+        //given
+        AccountUser Pobi = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(Pobi)); // 포비
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountUser(Pobi) // 포비
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .balance(0L)
+                        .accountNumber("1000000012").build()));
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        //then
+        assertEquals(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("거래 금액이 잔액보다 큰 경우")
+    void exceedAmount_UseBalance() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+
+        // 따로 뽑은 계좌 정보
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(IN_USE)
+                .balance(100L) // 100원 밖에 없다
+                .accountNumber("1000000012").build();
+
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+
+
+
+        //when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        //then
+        assertEquals(ErrorCode.AMOUNT_EXCEED_BALANCE, exception.getErrorCode());
+        verify(transactionRepository, times(0)).save(any());
+    }
+
+    @Test
+    @DisplayName("실패 트랜잭션 저장 성공")
+    void saveFailedUseTransaction() {
+        //given
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+
+        // 따로 뽑은 계좌 정보
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(IN_USE)
+                .balance(10000L)
+                .accountNumber("1000000012").build();
+
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(USE)
+                        .transactionResultType(S)
+                        .transactionId("transactionId")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapshot(9000L)
+                        .build());
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        //when
+        transactionService.saveFailedUseTransaction("1000000000", 200L);
+
+        //then 10000원에서 1000원 썼으니 9000원 일것이다
+        verify(transactionRepository, times(1)).save(captor.capture());
+        assertEquals(200L, captor.getValue().getAmount());
+        assertEquals(10000L, captor.getValue().getBalanceSnapshot()); // 결제 실패했으니 그대로
+        assertEquals(F, captor.getValue().getTransactionResultType()); // 성공여부
+    }
+}
